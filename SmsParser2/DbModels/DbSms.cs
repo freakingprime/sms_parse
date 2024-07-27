67167b54d49a118b9832aa839b2c9e9ef4146c27
Pham Phuong Bac <somethingnotright123@outlook.com>
2024-07-27 23:36:36 +0700

    Add DB models

---
+++ SmsParser2/DbModels/DbSms.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmsParser2.DbModels
+{
+    public class DbSms
+    {
+        public DbSms()
+        {
+        }
+
+        public int ID;
+        public string Address;
+        public string Body;
+        public DateTime Date;
+        public string ContactName;
+        public int Type;
+    }
+}
---
+++ SmsParser2/DbModels/DbTransaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmsParser2.DbModels
+{
+    public class DbTransaction
+    {
+        public DbTransaction()
+        {
+        }
+
+        public int ID;
+        public string BankName;
+        public DateTime Date;
+        public long Delta;
+        public long Balance;
+        public string TimeString;
+        public int SmsID;
+        public string Ref;
+    }
+}
